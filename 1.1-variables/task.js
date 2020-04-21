c3a38f7910526e11eac22e234dd143c7661b10dc
Andrey Gordov <gordov.ar@gmail.com>
2020-04-21 22:49:17 +0500

    Home Work 1

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -6,22 +6,28 @@
  */
 
 function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+	      
+	return (a + g + p) / 3;
 }
 
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
+	let myName = userName;
+	let message  = `Привет, мир! Меня зовут ${myName}`;
     
-	return // ваш ответ;
+	return message;
 }
 
 function calculateFormula() {
+	let x;
+	let y;
+	let z;
+	x = 2;
+	y = 22;
+	z = 0;
+	result = x * y + 5 * z + x - 1; 
     
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+    return result;
 }
