67f0545ef0076ae46ef23e5b389f8d79c12e694c
Andrey Gordov <gordov.ar@gmail.com>
2020-04-28 12:11:20 +0500

    Домашнее задание 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,48 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+    let x = [];
+    let diskrim = Math.pow(b,2) - 4 * a * c;
+
+    if (diskrim < 0) {
+        x = x; //сделано для прохождения теста. было x.push("Нет действительных корней"), что не является пустым массивом
+    }else if (diskrim == 0) {
+        x.push(-b / (2  * a));
+    }else {
+        x.push((-b + Math.sqrt(diskrim)) / (2 * a));
+        x.push((-b - Math.sqrt(diskrim)) / (2 * a));
+    }
+
+    return x;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+    let averageMark;
+    if (marks.length == 0) {
+        averageMark = 0;
+    }else {
+        if (marks.length > 5) {
+        console.log("Внимание, содержится более 500 оценок. К зачету идут первые 5");
+        marks.splice(5);
+        }
+      let sum = 0;
+      for (let i = 0; i < marks.length; i++){
+        sum += marks[i];
+      }
+        averageMark = sum / marks.length;
+    }
+
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let age;
+    age = new Date().getFullYear() - dateOfBirthday.getFullYear();
+
+    if (age >= 18){
+      result = `Не желаете ли олд-фэшн, ${name}?`;
+    }else {
+      result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+
+    return result;
 }
