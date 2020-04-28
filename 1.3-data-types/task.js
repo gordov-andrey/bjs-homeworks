1e45a02668b7cb6ba5ffa01be7493d44d385514f
Andrey Gordov <gordov.ar@gmail.com>
2020-04-28 21:45:18 +0500

    Домашнее задание 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,26 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    percent = new Number(percent);
+    contribution = new Number(contribution);
+    amount = new Number(amount);
+    let creditBody = amount - contribution; //тело кредита
+    let numOfMonths = (date.getFullYear() - new Date().getFullYear()) * 12; //количество месяцев
+    let pcnt = (percent / 12) / 100; //процентики
+    let q = Math.pow(1 + pcnt, numOfMonths); //кусок формулы
+    let mounthlyPayment = creditBody * pcnt * q / (q - 1); //расчет платежа
+    let totalAmount = mounthlyPayment * numOfMonths; 
+    totalAmount = new Number(totalAmount.toFixed(2)); //оставляем 2 знака после запятой, приводим строку к числу
+
+    console.log(totalAmount);
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name == "" || name == null || name == undefined){
+        name = "Аноним";
+    } 
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+
+    console.log(greeting);
+    return greeting;
 }
